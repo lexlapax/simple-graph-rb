1f3de430f5ce164536206f133e4481c44af39102
Lex Lapax <lexlapax@gmail.com>
2022-09-15 20:36:25 -0700

    changed create_Sql.rb to be run from command line

--- sql/create_sql.rb
+++ sql/create_sql.rb
@@ -1,15 +1,17 @@
-sqlfile = "json_graph_sqlite3.sql"
-sqlfiledir="#{ENV['HOME']}/projects/per/simple-graph-rb/simple-graph/sql"
+## run file like
+## bundle exec ruby sql/create_sql.rb simple-graph/sql >> sql/json_graph_sqlite3.sql
+#sqlfile = "json_graph_sqlite3.sql"
+sqlfiledir=ARGV[0] #"simple-graph/sql"
 dirs=Dir.children(sqlfiledir)
-File.open(sqlfile, 'w+') do |fh|
-    fh.write("-- timestamp #{Time.now}\n")
-    fh.write("-- combining sql files from #{sqlfiledir}\n")
+# File.open(sqlfile, 'w+') do |fh|
+    puts("-- timestamp #{Time.now}\n")
+    puts("-- combining sql files from #{sqlfiledir}\n")
     dirs.sort.each do |filename|
         if filename.end_with? ".sql" 
             file = File.read(sqlfiledir + "/" + filename)
-            fh.write("-- #{filename}\n")
-            fh.write(file)
-            fh.write("\n\n")
+            puts("-- #{filename}\n")
+            puts(file)
+            puts("\n\n")
         end
     end
-end
+# end
